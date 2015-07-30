984f23c3ee942b25ad430920f7fcc4c16e0e362c
Brian Ip <brianip@squareup.com>
2015-07-30 09:01:43 -0700

    ensure resp.Body is non nil

--- src/github.com/outbrain/orchestrator-agent/agent/agent.go
+++ src/github.com/outbrain/orchestrator-agent/agent/agent.go
@@ -57,10 +57,10 @@ func SubmitAgent() error {
 	log.Debugf("Submitting this agent: %s", url)
 
 	response, err := httpGet(url)
-	defer response.Body.Close()
 	if err != nil {
 		return log.Errore(err)
 	}
+	defer response.Body.Close()
 
 	log.Debugf("response: %+v", response)
 	return err
